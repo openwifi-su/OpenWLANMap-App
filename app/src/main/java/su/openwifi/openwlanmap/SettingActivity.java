dedb8f12eb9b71a0bad777013096cbaaabce6b29
lilli <caothivananh98@gmail.com>
2018-06-09 16:24:11 +0200

    add menu in setting (import/export/reset)

--- app/src/main/java/su/openwifi/openwlanmap/SettingActivity.java
+++ app/src/main/java/su/openwifi/openwlanmap/SettingActivity.java
@@ -2,6 +2,9 @@ package su.openwifi.openwlanmap;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
     @Override
@@ -9,5 +12,32 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting_reset:
+                Toast.makeText(this, "Settings is reseted sucessfully", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.setting_export:
+                Toast.makeText(this, "Settings is exported sucessfully", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.setting_import:
+                Toast.makeText(this, "Settings is imported sucessfully", Toast.LENGTH_LONG).show();
+                break;
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
