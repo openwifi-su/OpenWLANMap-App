79fbe3ec6be5bb143a407397b30630c5eff656da
lilli <caothivananh98@gmail.com>
2018-06-09 13:58:33 +0200

    add news activity demo

--- app/src/main/java/su/openwifi/openwlanmap/NewsActivity.java
+++ app/src/main/java/su/openwifi/openwlanmap/NewsActivity.java
@@ -1,7 +1,8 @@
 package su.openwifi.openwlanmap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class NewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
