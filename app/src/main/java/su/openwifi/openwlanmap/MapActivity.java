7386afc8b208aad68e0d480eafdc2790e638a1ee
lilli <caothivananh98@gmail.com>
2018-06-09 14:00:23 +0200

    add show map activity demo

--- app/src/main/java/su/openwifi/openwlanmap/MapActivity.java
+++ app/src/main/java/su/openwifi/openwlanmap/MapActivity.java
@@ -1,7 +1,9 @@
 package su.openwifi.openwlanmap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        Toast.makeText(this, "This is only a demo map, the real map will show your current position as well", Toast.LENGTH_LONG)
+                .show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
