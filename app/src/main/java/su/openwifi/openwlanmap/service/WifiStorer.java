c1f434e90c608f565baac284fbfd70b72f490421
lilli <caothivananh98@gmail.com>
2018-06-09 16:45:58 +0200

    add empty wifi storer and updater

--- app/src/main/java/su/openwifi/openwlanmap/service/WifiStorer.java
+++ app/src/main/java/su/openwifi/openwlanmap/service/WifiStorer.java
@@ -4,5 +4,9 @@ package su.openwifi.openwlanmap.service;
  * Created by tali on 01.06.18.
  */
 
-public class WifiStorer {
+public class WifiStorer implements Runnable {
+    @Override
+    public void run() {
+        //Do storing data in local disk
+    }
 }
--- app/src/main/java/su/openwifi/openwlanmap/service/WifiUploader.java
+++ app/src/main/java/su/openwifi/openwlanmap/service/WifiUploader.java
@@ -1,8 +1,19 @@
 package su.openwifi.openwlanmap.service;
 
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
 /**
  * Created by tali on 31.05.18.
  */
 
-public class WifiUploader {
+public class WifiUploader extends AsyncTaskLoader {
+    public WifiUploader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Object loadInBackground() {
+        return null;
+    }
 }
