231d4d9552ae9761424f94aec1c1aceafa4ebf5d
lilli <caothivananh98@gmail.com>
2018-07-08 22:55:42 +0200

    add config class

--- app/src/main/java/su/openwifi/openwlanmap/service/Config.java
+++ app/src/main/java/su/openwifi/openwlanmap/service/Config.java
@@ -1,4 +1,18 @@
 package su.openwifi.openwlanmap.service;
 
+/**
+ * This class contains all the config needed to control the
+ * scanning service.
+ */
 public class Config {
+  //TODO how to let storer thread update data/ or stop it and start new thread for it
+  private static boolean isUploading = false;
+
+  public static synchronized boolean getUploadStatus() {
+    return isUploading;
+  }
+
+  public static synchronized void setUploadStatus(boolean uploadStatus) {
+    isUploading = uploadStatus;
+  }
 }
